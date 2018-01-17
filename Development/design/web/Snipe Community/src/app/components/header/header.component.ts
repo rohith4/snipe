e9290ff1dda74b478dee9a2dc2f87f345e339b76
dhareeshsk <32480741+dhareeshsk@users.noreply.github.com>
2018-01-17 17:25:58 +0530

    Add files via upload

--- Development/design/web/Snipe Community/src/app/components/header/header.component.ts
+++ Development/design/web/Snipe Community/src/app/components/header/header.component.ts
@@ -1,28 +1,31 @@
 import { Component, OnInit,Inject  } from '@angular/core';
 import { NgForm }    from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import {Observable} from 'rxjs/Observable';
 import { Router }  from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { UserService } from './../../services/user.service';
-
+import { Component }           from '@angular/core';
+import { UserService } from '../../services/user.service'
 import 'rxjs/Rx';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
-})
+  
+  })
 export class HeaderComponent implements OnInit {
-  form: FormGroup;                    // {1}
 
-  constructor(private fb: FormBuilder,private userService : UserService) { }
-
+  form: FormGroup;
+ constructor(private fb: FormBuilder,private userService : UserService){}
   ngOnInit() {
-    this.form = this.fb.group({     // {5}
-      tag: ['', Validators.required],
-      que: ['', Validators.required]
-    });
+     this.form = this.fb.group({    
+     tag: ['', ],
+      que: ['', ],
+      });
+    
   }
+  
+
   onSubmit() {
     this.userService.headerUser(this.form.value)
     .subscribe(
@@ -30,5 +33,14 @@ export class HeaderComponent implements OnInit {
       (error) => console.log(error),
       () => console.log('success')  
     );
+  
+  
+console.log("data in component"+JSON.stringify(this.form.value));
+     this.call();
+	 }
+ call() {
+   this.userService.headerUser(JSON.stringify(this.form.value));
+
   }
 }
+
